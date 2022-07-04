49d5320a292bf5ce9cb56e9b000a85d4e5125cf8
huydang-teq <huydang@teqnological.asia>
2022-07-04 17:23:06 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: `@import "@styles/base.module"; @import "@styles/common.mixin";`,
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import style from "@styles/pages/Home.module.scss";
+const Home: NextPage = () => {
+  return <h1 className={style.heading}>Heading</h1>;
+};
+
+export default Home;
